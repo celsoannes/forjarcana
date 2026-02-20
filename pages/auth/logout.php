5075e14e2339ae564ac1d870cfcb1674d1d3ffef
Celso Annes <celsoannes@gmail.com>
2026-02-20 19:04:59 -0300

    ajustado os cookies

--- pages/auth/logout.php
+++ pages/auth/logout.php
@@ -2,6 +2,10 @@
 // filepath: /var/www/html/forjarcana/pages/auth/logout.php
 
 session_start();
+require_once __DIR__ . '/../../app/db.php';
+require_once __DIR__ . '/../../app/auth.php';
+
+revogar_login_lembrado_atual($pdo);
 session_unset();
 session_destroy();
 header('Location: ../../app/login.php');
